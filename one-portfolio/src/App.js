f3ef7febe3b781f6e83c804e66d88b83f95a6db9
Achal <achalghugare29aug@gmail.com>
2024-07-01 23:02:33 +0530

    upload protfolioproject

---
+++ one-portfolio/src/App.js
@@ -0,0 +1,24 @@
+import { About } from "./Components/About";
+import Contact from "./Components/Conact";
+import { Experience } from "./Components/Experience";
+import { Home } from "./Components/Home";
+import { NavBar } from "./Components/NavBar";
+import ScoialLinks from "./Components/ScoialLinks";
+
+function App() {
+  return (
+    <div >
+      
+      <NavBar/>
+    
+<Home/>
+<About/>
+<Experience/>
+<Contact/>
+<ScoialLinks/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ one-portfolio/src/Components/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b
+    from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+        </div>
+        <p className='text-xl mt-20'>Hey there!, I am Achal, a  java Full Stack Developer with a professional
+           responsible for designing, developing, functional, and user-centered digital experiences.I am always looking for
+            new and innovative ways to bring my clients visions to life.
+
+I just making things look pretty its about solving problems and creating intuitive, enjoyable experiences for users..</p>
+              <br/>
+              <p className='text-xl'>A Java Full Stack Developer is a professional responsible for designing, developing, and maintaining both the front-end 
+                and back-end components of web applications using Java and related technologies.
+                  </p>
+    </div>
+    </div>
+  )
+}
